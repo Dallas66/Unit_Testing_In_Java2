c2b3860144ddf55cc93d2ef441f5f7f6d8cf6516
Dallas66 <protasov001@gmail.com>
2018-06-09 17:13:42 +0400

    1.0

--- src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
+++ src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
@@ -1,16 +1,13 @@
 package edu.epam.izhevsk.junit;
 
-import org.hamcrest.CustomMatcher;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
+
 import static org.mockito.AdditionalMatchers.*;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
+import org.mockito.*;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 
@@ -18,5 +15,47 @@ import static org.mockito.Mockito.*;
 
 public class PaymentControllerTest {
 
- 
+    @InjectMocks
+    private PaymentController paymentController;
+    @Mock
+    private DepositService depositService = mock(DepositService.class);
+    @Mock
+    private AccountService accountService = mock(AccountService.class);
+    private long id = 100L;
+    private long wrongId = 51L;
+    private long amount = 50L;
+    private long wrongAmount = 101L;
+
+
+    @Before
+    public void init() throws InsufficientFundsException {
+        MockitoAnnotations.initMocks(this);
+
+        when(accountService.isUserAuthenticated(id)).thenReturn(true);
+        when(depositService.deposit(amount,id)).thenReturn("successful");
+        when(depositService.deposit(gt(100L), eq(100L))).thenThrow(new InsufficientFundsException());
+
+    }
+
+    @Test
+    public void validUser(){
+        assertTrue(accountService.isUserAuthenticated(id));
+    }
+
+    @Test
+    public void notValidUser(){
+        assertFalse(accountService.isUserAuthenticated(wrongId));
+    }
+
+    @Test(expected = InsufficientFundsException.class)
+    public void paymentControllerDepositTest() throws InsufficientFundsException {
+        paymentController.deposit(wrongAmount,id);
+    }
+
+    @Test(expected = SecurityException.class)
+    public void paymentControllerWrongIdTest() throws InsufficientFundsException {
+        paymentController.deposit(amount, wrongId);
+    }
+
+
 }
